afbc6ead89cf136678d0fb5f1945cda472ceadd8
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2024-08-07 22:15:30 -0700

    feat: add out-file arg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -7,8 +8,12 @@ struct Cli {
     command: String,
 
     /// How frequently, in seconds, should memory usage be recorded
-    #[arg(short = 'n', long)]
+    #[arg(short = 'n', long, default_value_t = 5)]
     interval: u8,
+
+    /// Where to write memory usage numbers
+    #[arg(short, long, value_name = "FILE")]
+    out_file: Option<PathBuf>,
 }
 
 fn main() {
